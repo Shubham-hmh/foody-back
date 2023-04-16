085ce1afeb6568875e8339e558e7a4d914360f47
shubham-hmh <shubham_203054@saitm.org>
2023-04-16 23:24:05 +0530

    cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose");
 const app =express();
 const port = process.env.PORT || 5000
 const path =require("path");
-
+const cors =require("cors");
 const mongoDBconnect =require('./db');
 mongoDBconnect();
 
@@ -12,6 +12,7 @@ mongoDBconnect();
 
 
 app.use(express.json());
+app.use(cors());
 
 
 
